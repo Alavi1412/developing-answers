1cc5851207575c6015bd288b5b37c2d68f3bb87b
Seyed Mohammad Hassan Alavi <alavi_hassan@yahoo.com>
2017-04-06 12:37:17 +0430

    right => left

--- plugin.rb
+++ plugin.rb
@@ -6,6 +6,6 @@ register_asset 'stylesheets/user-widget.scss'
 
 after_initialize do
   SiteSetting.class_eval do
-    @choices[:layouts_sidebar_right_widgets].push('developing-solution')
+    @choices[:layouts_sidebar_left_widgets].push('developing-solution')
   end
 end
